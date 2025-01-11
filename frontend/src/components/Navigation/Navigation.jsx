644bd3323a79e0783c817757376fc0d870e61246
MehwishHanif <mehwish.hanif90@gmail.com>
2025-01-10 16:37:38 -0800

    added styling

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -7,7 +7,7 @@ function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
 
   return (
-    <ul>
+    <ul className='nav-wrapper'>
       <li>
         <NavLink to="/">Home</NavLink>
       </li>
